476d714e1f2b2eff4f68d12636f13f548805ec39
Binh Lam <cindybinh444@gmail.com>
2018-09-20 14:53:33 -0400

    comment js codes

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,4 @@
-//api key
+//API key for Recipe Search API
 
 var apiKey = "ad9411943fc77cb3dcb9f5c1f72654de";
 var apiId = "b3505691";
@@ -6,12 +6,13 @@ var apiId = "b3505691";
 //Declare variables
 
 var searchTerm = "";
+var searchItems = [];
 var numIngredients = 0;
 var numResults = 0;
 var numTime = 0;
 var chosenSearch = [];
 
-// Initialize Firebase
+// Initialize Firebase - 'bootcamp-for admin reference'
 var config = {
   apiKey: "AIzaSyC9DKpx43Vp_crjyB6Auv2i9QyS2vp5ztI",
   authDomain: "my-project-bb79e.firebaseapp.com",
@@ -24,16 +25,14 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-//URL base
+//queryURL base
 var queryURLBase =
   "http://api.edamam.com/search?from=0&to=20&app_id=" +
   apiId +
   "&app_key=" +
   apiKey;
 
-//track number of recipe
-recipeCounter = 0;
-
+//Get the data from the Recipe Search API
 function runQuery(numSearch, queryURL) {
   $.ajax({
     url: queryURL,
@@ -41,6 +40,7 @@ function runQuery(numSearch, queryURL) {
   }).done(function(response) {
     $("#displayResults").empty();
 
+    //Logic for cook/prep time
     if (parseInt(numTime) === 30) {
       response.hits.forEach(function(element) {
         if (
@@ -75,27 +75,29 @@ function runQuery(numSearch, queryURL) {
         }
       });
     }
-
+    //Display Search Results
     chosenSearch.slice(0, numSearch).forEach(function(element, i) {
-
       var calories = Math.round(element.calories / element.yield);
 
+      //Display Card Section for Each Recipe Result
       var displaySection = $("<div>");
       displaySection.attr("id", "recipe-" + i);
-      displaySection.addClass("card p-3 m-2 verticalList");
+      displaySection.addClass("card p-3 m-2");
+      $("#displayResults").append(displaySection);
 
+      //Display Videos of Each Recipe Result
       var displayVideo = $("<div>");
-      displayVideo.attr("id","player-"+i);
+      displayVideo.attr("id", "player-" + i);
       displayVideo.addClass("m-2");
 
       var displayVideo1 = $("<div>");
-      displayVideo1.attr("id","player1-"+i);
+      displayVideo1.attr("id", "player1-" + i);
       displayVideo1.addClass("m-2");
 
       var displayVideo2 = $("<div>");
-      displayVideo2.attr("id","player2-"+i);
+      displayVideo2.attr("id", "player2-" + i);
       displayVideo2.addClass("m-2");
-
+      //Video Container for the above videos
       var videoSection = $("<div>");
       videoSection.attr("id", "videoSection-" + i);
       videoSection.append(displayVideo);
@@ -115,7 +117,7 @@ function runQuery(numSearch, queryURL) {
       youtubeBtn.text("Show Sample Videos");
       youtubeBtn.addClass("videoBtn w-25 m-3");
 
-      //Save to collection Button
+      //Save to My Recipes Button
       var saveBtn = $("<button>");
       saveBtn.attr("data-title", element.label);
       saveBtn.attr("data-image", element.image);
@@ -134,9 +136,7 @@ function runQuery(numSearch, queryURL) {
       buttonSection.append(youtubeBtn);
       buttonSection.append(saveBtn);
 
-      $("#displayResults").append(displaySection);
-
-      //Attach the content to the appropriate div
+      //Attach the Recipe Result content to the appropriate Card Section Div
       $("#recipe-" + i).append("<h3>" + element.label.toUpperCase() + "</h3>");
       $("#recipe-" + i).append(
         "<h5>" +
@@ -159,19 +159,18 @@ function runQuery(numSearch, queryURL) {
       );
       $("#recipe-" + i).append(buttonSection);
       $("#recipe-" + i).append(videoSection);
-      
     });
   });
 }
-var searchItems = [];
 
+//Search Recipe Button
 $("#searchBtn").on("click", function() {
   chosenSearch = [];
 
   searchTerm = $("#searchTerm")
     .val()
     .trim();
-
+  //push all the searchTerm string to the searchItems Array
   searchItems.push(searchTerm);
 
   numTime = $("#numTime").val();
@@ -181,21 +180,23 @@ $("#searchBtn").on("click", function() {
   numResults = $("#numResults").val();
 
   searchItems.push(numResults);
-
+  //store all the searchItems into Local Storage for later access
   localStorage.setItem("searchItems", JSON.stringify(searchItems));
 
   var newURL = queryURLBase + "&q=" + searchTerm;
-
+  //Run this function to GET the Recipe Search API data
   runQuery(parseInt(numResults), newURL);
-
+  //Allow default behavior in order to take the browser to a new page
   return true;
 });
 
+//Instruction Button opens up a new page in a new window
 $(document).on("click", ".instructionBtn", function() {
   var url = $(this).attr("src");
   window.open(url);
 });
 
+//Save Button allows to get all the Recipe Data and store them inside of Firebase
 $(document).on("click", ".saveBtn", function() {
   var title = $(this).attr("data-title");
   var image = $(this).attr("data-image");
@@ -203,7 +204,7 @@ $(document).on("click", ".saveBtn", function() {
   var dietLabel = $(this).attr("data-dietLabel");
   var healthLabel = $(this).attr("data-healthLabel");
   var url = $(this).attr("data-url");
-
+  //push data to Firebase database
   database.ref().push({
     title: title,
     image: image,
@@ -215,13 +216,17 @@ $(document).on("click", ".saveBtn", function() {
   });
 });
 
+//Array to store video IDs -- in order to choose how many videos to display
 var videoIdArray = [];
-
+//Attach Youtube IFrame player API codes to display youtube videos
+//This code loads the IFrame Player API code asynchronously
 var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+//GET Youtube Data API data --- specifically 'videoId'
+//Insert the videoIds (found in Data API) into the Iframe player API to display
 $(document).on("click", ".videoBtn", function() {
   videoIdArray = [];
   var name = $(this).attr("name");
@@ -230,67 +235,52 @@ $(document).on("click", ".videoBtn", function() {
     "https://www.googleapis.com/youtube/v3/search?q=" +
     name +
     "&part=snippet&key=AIzaSyAcwzTei_3ijB48GQzlph93ht_rExhGKM4";
-    
+
   $.ajax({
     url: queryURL,
     method: "GET"
   })
     .done(function(result) {
-
-
+      //push the videoId into the videoIdArray to store
       result.items.forEach(function(element) {
-
         videoIdArray.push(element.id.videoId);
-
       });
-
+      //See Console for this array to know what it stores
       console.log(videoIdArray);
 
+      //This function creates an <iframe> (and YouTube player) after the API code downloads
       function onYouTubeIframeAPIReady() {
-        player = new YT.Player("player-"+index, {
+        player = new YT.Player("player-" + index, {
           height: "150",
-          width: "300", 
+          width: "300",
           videoId: videoIdArray[0],
           events: {
-            'onReady': onPlayerReady
+            onReady: onPlayerReady
           }
         });
-        
       }
 
       function onYouTubeIframeAPIReady1() {
-        player1 = new YT.Player("player1-"+index, {
+        player1 = new YT.Player("player1-" + index, {
           height: "150",
           width: "300",
           videoId: videoIdArray[1],
           events: {
-            'onReady': onPlayerReady
+            onReady: onPlayerReady
           }
         });
       }
 
       function onYouTubeIframeAPIReady2() {
-        player2 = new YT.Player("player2-"+index, {
+        player2 = new YT.Player("player2-" + index, {
           height: "150",
           width: "300",
           videoId: videoIdArray[2],
           events: {
-            'onReady': onPlayerReady
+            onReady: onPlayerReady
           }
         });
       }
-
-      // function onYouTubeIframeAPIReady3() {
-      //   player3 = new YT.Player("player3-"+index, {
-      //     height: "150",
-      //     width: "300",
-      //     videoId: videoIdArray[3],
-      //     events: {
-      //       'onReady': onPlayerReady
-      //     }
-      //   });
-      // }
-
       // The API will call this function when the video player is ready.
       var done = false;
       function onPlayerReady(event) {
@@ -299,14 +289,12 @@ $(document).on("click", ".videoBtn", function() {
           done = true;
         }
       }
-
+      //Call on the function to display the videos
       onYouTubeIframeAPIReady();
       onYouTubeIframeAPIReady1();
       onYouTubeIframeAPIReady2();
-      // onYouTubeIframeAPIReady3()
     })
     .fail(function(err) {
       throw err;
     });
-
 });
--- javascript/displayapp.js
+++ javascript/displayapp.js
@@ -1,9 +1,4 @@
-//Access to Local Storage 
-var array = JSON.parse(localStorage.getItem("searchItems"));
-console.log(array);
-console.log("First item:" + array[0], "Second item:" + array[1], "Third item:" + array[2]);
-
-//api key
+//API key for Recipe Search API
 
 var apiKey = "ad9411943fc77cb3dcb9f5c1f72654de";
 var apiId = "b3505691";
@@ -16,7 +11,7 @@ var numResults = 0;
 var numTime = 0;
 var chosenSearch = [];
 
-// Initialize Firebase
+// Initialize Firebase - 'bootcamp-for admin reference'
 var config = {
   apiKey: "AIzaSyC9DKpx43Vp_crjyB6Auv2i9QyS2vp5ztI",
   authDomain: "my-project-bb79e.firebaseapp.com",
@@ -29,19 +24,25 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-//URL base
+//queryURL base
 var queryURLBase =
   "http://api.edamam.com/search?from=0&to=20&app_id=" +
   apiId +
   "&app_key=" +
   apiKey;
 
-//track number of recipe
-recipeCounter = 0;
+//Access to Local Storage
+var array = JSON.parse(localStorage.getItem("searchItems"));
+console.log(array);
+console.log(
+  "First item:" + array[0],
+  "Second item:" + array[1],
+  "Third item:" + array[2]
+);
 
-
-
-searchTerm =  array[0];
+//Search Button has already removed for the recipes.html page
+//and so the search button function is also removed
+searchTerm = array[0];
 
 numTime = parseInt(array[1]);
 
@@ -49,10 +50,10 @@ numResults = array[2];
 
 var newURL = queryURLBase + "&q=" + searchTerm;
 
+//Call the runQuery function 
 runQuery(parseInt(numResults), newURL);
 
-
-
+//Get the data from the Recipe Search API
 function runQuery(numSearch, queryURL) {
   $.ajax({
     url: queryURL,
@@ -60,6 +61,7 @@ function runQuery(numSearch, queryURL) {
   }).done(function(response) {
     $("#displayResults").empty();
 
+    //Logic for cook/prep time
     if (parseInt(numTime) === 30) {
       response.hits.forEach(function(element) {
         if (
@@ -94,36 +96,35 @@ function runQuery(numSearch, queryURL) {
         }
       });
     }
-
+    //Display Search Results
     chosenSearch.slice(0, numSearch).forEach(function(element, i) {
-
       var calories = Math.round(element.calories / element.yield);
 
+      //Display Card Section for Each Recipe Result
       var displaySection = $("<div>");
       displaySection.attr("id", "recipe-" + i);
       displaySection.addClass("card p-3 m-2 verticalList");
-
+      
+      //Display Videos of Each Recipe Result
       var displayVideo = $("<div>");
-      displayVideo.attr("id","player-"+i);
+      displayVideo.attr("id", "player-" + i);
       displayVideo.addClass("m-2");
 
       var displayVideo1 = $("<div>");
-      displayVideo1.attr("id","player1-"+i);
+      displayVideo1.attr("id", "player1-" + i);
       displayVideo1.addClass("m-2");
 
       var displayVideo2 = $("<div>");
-      displayVideo2.attr("id","player2-"+i);
+      displayVideo2.attr("id", "player2-" + i);
       displayVideo2.addClass("m-2");
-
+      //Video Container for the above videos
       var videoSection = $("<div>");
       videoSection.attr("id", "videoSection-" + i);
       videoSection.append(displayVideo);
       videoSection.append(displayVideo1);
       videoSection.append(displayVideo2);
-      
 
-      //*********Modal display code for videoSection****** */
-
+      //Modal display code for videoSection
       var modal_Div = $("<div>");
       modal_Div.addClass("modal modal-fixed-footer");
       modal_Div.attr("id", "modal-" + i);
@@ -167,7 +168,7 @@ function runQuery(numSearch, queryURL) {
         "videoBtn waves-effect waves-light btn modal-trigger"
       );
 
-      //Save to collection Button
+      //Save to My Recipes Button
       var saveBtn = $("<button>");
       saveBtn.attr("data-title", element.label);
       saveBtn.attr("data-image", element.image);
@@ -188,7 +189,7 @@ function runQuery(numSearch, queryURL) {
 
       $("#displayResults").append(displaySection);
 
-      //Attach the content to the appropriate div
+      //Attach the Recipe Result content to the appropriate Card Section Div
       $("#recipe-" + i).append("<h3>" + element.label.toUpperCase() + "</h3>");
       $("#recipe-" + i).append(
         "<h5>" +
@@ -210,17 +211,17 @@ function runQuery(numSearch, queryURL) {
         "<h5> Total Calories/person: " + calories + "</h5>"
       );
       $("#recipe-" + i).append(buttonSection);
-      
     });
   });
 }
 
-
+//Instruction Button opens up a new page in a new window
 $(document).on("click", ".instructionBtn", function() {
   var url = $(this).attr("src");
   window.open(url);
 });
 
+//Save Button allows to get all the Recipe Data and store them inside of Firebase
 $(document).on("click", ".saveBtn", function() {
   var title = $(this).attr("data-title");
   var image = $(this).attr("data-image");
@@ -228,7 +229,7 @@ $(document).on("click", ".saveBtn", function() {
   var dietLabel = $(this).attr("data-dietLabel");
   var healthLabel = $(this).attr("data-healthLabel");
   var url = $(this).attr("data-url");
-
+  //push data to Firebase database
   database.ref().push({
     title: title,
     image: image,
@@ -240,13 +241,17 @@ $(document).on("click", ".saveBtn", function() {
   });
 });
 
+//Array to store video IDs -- in order to choose how many videos to display
 var videoIdArray = [];
-
+//Attach Youtube IFrame player API codes to display youtube videos
+//This code loads the IFrame Player API code asynchronously
 var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+//GET Youtube Data API data --- specifically 'videoId'
+//Insert the videoIds (found in Data API) into the Iframe player API to display
 $(document).on("click", ".videoBtn", function() {
   videoIdArray = [];
   var name = $(this).attr("name");
@@ -255,56 +260,52 @@ $(document).on("click", ".videoBtn", function() {
     "https://www.googleapis.com/youtube/v3/search?q=" +
     name +
     "&part=snippet&key=AIzaSyAcwzTei_3ijB48GQzlph93ht_rExhGKM4";
-    
+
   $.ajax({
     url: queryURL,
     method: "GET"
   })
     .done(function(result) {
-
-
+      //push the videoId into the videoIdArray to store
       result.items.forEach(function(element) {
-
         videoIdArray.push(element.id.videoId);
-
       });
-
+      //See Console for this array to know what it stores
       console.log(videoIdArray);
 
+      //This function creates an <iframe> (and YouTube player) after the API code downloads
       function onYouTubeIframeAPIReady() {
-        player = new YT.Player("player-"+index, {
+        player = new YT.Player("player-" + index, {
           height: "150",
-          width: "300", 
+          width: "300",
           videoId: videoIdArray[0],
           events: {
-            'onReady': onPlayerReady
+            onReady: onPlayerReady
           }
         });
-        
       }
 
       function onYouTubeIframeAPIReady1() {
-        player1 = new YT.Player("player1-"+index, {
+        player1 = new YT.Player("player1-" + index, {
           height: "150",
           width: "300",
           videoId: videoIdArray[1],
           events: {
-            'onReady': onPlayerReady
+            onReady: onPlayerReady
           }
         });
       }
 
       function onYouTubeIframeAPIReady2() {
-        player2 = new YT.Player("player2-"+index, {
+        player2 = new YT.Player("player2-" + index, {
           height: "150",
           width: "300",
           videoId: videoIdArray[2],
           events: {
-            'onReady': onPlayerReady
+            onReady: onPlayerReady
           }
         });
       }
-
 
       // The API will call this function when the video player is ready.
       var done = false;
@@ -315,13 +316,12 @@ $(document).on("click", ".videoBtn", function() {
         }
       }
 
+      //Call on the function to display the videos
       onYouTubeIframeAPIReady();
       onYouTubeIframeAPIReady1();
       onYouTubeIframeAPIReady2();
-      
     })
     .fail(function(err) {
       throw err;
     });
-
 });
